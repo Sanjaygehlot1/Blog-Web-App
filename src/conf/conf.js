10b7a0b84adf779501e625095e9eab2cc16b6377
Sanjaygehlot1 <165627993+Sanjaygehlot1@users.noreply.github.com>
2025-01-02 14:54:44 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
     APPWRITE_ENDPOINT :"https://cloud.appwrite.io/v1" ,
-    PROJECT_ID : "67627334002b344da3c0",
-    COLLECTION_ID : "676273fb002ee940967c",
-    DATABASE_ID : "676273e700122b697346",
-    BUCKET_ID : "67627566002e64362278",
+    PROJECT_ID : "your-project-id",
+    COLLECTION_ID : "your-collection-id",
+    DATABASE_ID : "your-database-id",
+    BUCKET_ID : "your-bucket-id",
 
 }
 export default conf
